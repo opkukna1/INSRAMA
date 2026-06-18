c375548af799c7723b424f112f9cc7f0210262ad
opkukna1 <Opsiddh41@gmail.com>
2026-06-18 21:16:45 +0530

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/com/example/ins_rama/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.ins_rama
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // यहाँ किसी भी पुराने FlutterActivityDelegate या एम्बेडिंग कोड की ज़रूरत नहीं है
+}
